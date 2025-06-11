2dd9a1b3508e29275da4cd638563608abbedc392
yebe <yebe@multiped.com>
2025-06-10 23:05:08 -0500

    updating motor command

--- ros_mtr_cmd_sub.py
+++ ros_mtr_cmd_sub.py
@@ -165,7 +165,7 @@ class MotorControl:
             try:
                 self.bus.send(can.Message(
                     arbitration_id=(index << 5 | 0x0C),  # Uses correct motor ID
-                    data=struct.pack('<fff', position[index], velocity_feedforward[index], torque_feedforward[index]),
+                    data=struct.pack('<fhh', position[index], int(velocity_feedforward[index] * 1000), int(torque_feedforward[index] * 1000)),
                     is_extended_id=False
                 ))
             except can.CanError as e:
